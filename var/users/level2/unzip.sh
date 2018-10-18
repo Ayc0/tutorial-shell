de028186a1f8e4a4cd34d9e17b41fcbd188d1b91
Ayc0 <ayc0.benj@gmail.com>
2018-10-18 23:23:50 +0200

    Avoid deleting .part.* if the zip command fails

--- var/users/level2/unzip.sh
+++ var/users/level2/unzip.sh
@@ -1,4 +1,20 @@
 #!/bin/zsh
 
-cat password.tar.part.* | tar jx
-rm -rf password.tar.part.*
+if [ -f password ]; then
+  echo "Cannot override file \"password\""
+  exit 1
+fi
+
+if [ -f password.tar.part.d ]; then
+  cat password.tar.part.* | tar jx
+  if [ -f password ]; then
+    rm -rf password.tar.part.*
+    echo "The parts are decompressed into one unique file \"password\""
+  fi
+else
+  echo "You have to find the missing part"
+  exit 1
+fi
+
+
+
